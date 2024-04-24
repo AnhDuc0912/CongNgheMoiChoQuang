b8f96542e0416febdea4171fae21c1f98d399e91
anhduc0912 <phamquocanhduc2003hn@gmail.com>
2024-04-24 11:33:22 +0700

    Revert "ui change password dialog"
    This reverts commit 6fb0af9fe232dbc1db5e04854b0497b10c769941.

--- src/dialog/ChangePasswordModal.jsx
+++ src/dialog/ChangePasswordModal.jsx
@@ -1,10 +1,7 @@
-import { Visibility, VisibilityOff } from "@mui/icons-material";
 import {
   Avatar,
   Button,
   Dialog,
-  IconButton,
-  InputAdornment,
   Stack,
   TextField,
   Typography,
@@ -13,7 +10,6 @@ import { makeStyles } from "@mui/styles";
 import axios from "axios";
 import { useFormik } from "formik";
 import { enqueueSnackbar } from "notistack";
-import { useState } from "react";
 import * as Yup from "yup";
 
 const useStyles = makeStyles((theme) => ({
@@ -42,9 +38,6 @@ const useStyles = makeStyles((theme) => ({
 
 function ChangePasswordModal({ open, onClose }) {
   const classes = useStyles();
-  const [showPassword, setShowPassword] = useState(false);
-  const [showNewPassword, setNewShowPassword] = useState(false);
-  const [showNewPasswordConfirm, setNewShowPasswordConfirm] = useState(false);
 
 
   return (
@@ -76,76 +69,25 @@ function ChangePasswordModal({ open, onClose }) {
               Thông tin tài khoản
             </Typography>
             <TextField
+              id="fullName"
+              className={classes.formField}
+              label="Họ và tên"
               fullWidth
-              id="password"
-              label="Mật khẩu hiện tại"
-              // onChange={formik.handleChange}
-              // onBlur={formik.handleBlur}
-              // error={formik.errors.password && formik.touched.password}
-              // helperText={formik.errors.password}
-              // value={formik.values.password}
-              type={showPassword ? "text" : "password"}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment>
-                    <IconButton
-                      onClick={() => {
-                        setShowPassword(!showPassword);
-                      }}
-                    >
-                      {showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
+              variant="outlined"
             />
             <TextField
+              id="email"
+              className={classes.formField}
+              label="Email"
               fullWidth
-              id="newPassword"
-              label="Mật khẩu mới"
-              // onChange={formik.handleChange}
-              // onBlur={formik.handleBlur}
-              // error={formik.errors.password && formik.touched.password}
-              // helperText={formik.errors.password}
-              // value={formik.values.password}
-              type={showNewPassword ? "text" : "password"}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment>
-                    <IconButton
-                      onClick={() => {
-                        setNewShowPassword(!showNewPassword);
-                      }}
-                    >
-                      {showNewPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
+              variant="outlined"
             />
             <TextField
+              id="phoneNumber"
+              className={classes.formField}
+              label="Điện thoại"
               fullWidth
-              id="newPassword"
-              label="Mật khẩu mới"
-              // onChange={formik.handleChange}
-              // onBlur={formik.handleBlur}
-              // error={formik.errors.password && formik.touched.password}
-              // helperText={formik.errors.password}
-              // value={formik.values.password}
-              type={showNewPasswordConfirm ? "text" : "password"}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment>
-                    <IconButton
-                      onClick={() => {
-                        setNewShowPasswordConfirm(!showNewPasswordConfirm);
-                      }}
-                    >
-                      {showNewPasswordConfirm ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
+              variant="outlined"
             />
             <Button type="submit" variant="contained" color="primary" fullWidth>
               Cập nhật
--- src/layouts/dashboard/Sidebar.jsx
+++ src/layouts/dashboard/Sidebar.jsx
@@ -44,7 +44,6 @@ const Sidebar = () => {
   const isSelected = (path) => location.pathname === path;
   const [anchorEl, setAnchorEl] = useState(null);
   const [openDialog, setOpenDialog] = useState(false);
-  const [openChangPWDialog, setOpenChangPWDialog] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -151,7 +150,7 @@ const Sidebar = () => {
           </ListItem>
 
           <ListItem
-            onClick={() => setOpenChangPWDialog(true)}
+            onClick={() => setOpenDialog(true)}
             button
             style={{ margin: "0 6px 0 6px" }}
           >
@@ -175,8 +174,8 @@ const Sidebar = () => {
       </Popover>
 
       <ChangePasswordModal
-        onClose={() => setOpenChangPWDialog(false)}
-        open={openChangPWDialog}
+        onClose={() => setOpenDialog(false)}
+        open={openDialog}
       />
       <ProfileModal onClose={() => setOpenDialog(false)} open={openDialog} />
     </Box>
